fd30291f1fc03b65c47b64b0b47bd4d1f978833c
charl <charlieslasluisa@gmail.com>
2022-01-24 14:55:40 +0100

    Test affichage ligne brisée

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,21 @@
-import test.Voler;
 
-import java.awt.Dimension;
+import java.awt.*;
 
 import javax.swing.JFrame;
 
 public class Main {
 
 	public static void main(String[] args) {
-		/*
-		JPanel composant = new JPanel(); //composant graphique 
-		JFrame fenetre = new JFrame("titre de la fenêtre"); //fenetre d'affichage 
-		fenetre.add(composant); //ajout du composant dans la fenetre
-		
-		fenetre.pack(); //assemblage des composants 
-		fenetre.setVisible(true); //on rend la fenetre visible
-		fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //fermeture de la fenetre graphique
-		
-		**/
-		
-		Affichage test = new Affichage();
-		Etat modele = new Etat(test);
-		Control ctrl = new Control(modele, test);
-		test.addMouseListener(ctrl);
-		new Voler(modele).start();
+		Parcours t = new Parcours();
+		for(Point p : t.getCoord()){
+			System.out.println(p.getX()+", "+p.getY());
+		}
+		Affichage aff = new Affichage();
+		Control ctrl = new Control(aff.getLiaison(), aff);
+		aff.addMouseListener(ctrl);
+		new Thread(new Voler(aff.getLiaison())).start();
 		JFrame frame = new JFrame("Fenetre principale");
-		frame.add(test);
+		frame.add(aff);
 		frame.pack();
 		frame.setSize(new Dimension(500, 500));
 		frame.setVisible(true); //on rend la fenetre visible
